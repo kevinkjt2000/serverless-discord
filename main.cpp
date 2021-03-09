a6a9d7e87478778acf7e6f0c08d746377e28cb33
Kevin Tindall <kevinkjt2000@gmail.com>
2021-03-08 23:17:24 -0600

    Parse json with nlohmann on lambda
    Much thanks to hunter for being an awesome package manager. I still need
    to fork aws-lambda-cpp to hunter-ize it, but for now setting HUNTER_ROOT
    to my fork of HUNTER seems to suffice.

--- main.cpp
+++ main.cpp
@@ -1,16 +1,23 @@
 #include <aws/lambda-runtime/runtime.h>
+#include <nlohmann/json.hpp>
 
 using namespace aws::lambda_runtime;
+using json = nlohmann::json;
 
 static invocation_response my_handler(invocation_request const& req)
 {
-    if (req.payload.length() > 42) {
-        return invocation_response::failure("error message here"/*error_message*/,
-                                            "error type here" /*error_type*/);
+    auto parsed_payload = json::parse(req.payload);
+    if (parsed_payload.contains("type") && parsed_payload["type"] == 1) {
+        auto response = R"(
+          {
+            "type": 1
+          }
+        )"_json;
+        return invocation_response::success(response.dump(), "application/json");
     }
 
-    return invocation_response::success(req.payload /*payload*/,
-                                        "application/json" /*MIME type*/);
+    return invocation_response::failure("unexpected non-ping payload received",
+                                        "error type here");
 }
 
 int main()
