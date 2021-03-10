5ff6f5941a5de1b0ae06c527313901e666964757
Kevin Tindall <kevinkjt2000@gmail.com>
2021-03-09 19:41:33 -0600

    Add api gateway that forwards stuff to c++ lambda
    (Even more stuff that I'll need to convert into C++ code if I want 100%
    C++)

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+variable "lambda_function_name" {
+  default = "lambda_function_name"
+}
+
 resource "aws_iam_role" "iam_for_lambda" {
   name = "iam_for_lambda"
 
@@ -8,7 +12,7 @@ resource "aws_iam_role" "iam_for_lambda" {
     {
       "Action": "sts:AssumeRole",
       "Principal": {
-        "Service": "lambda.amazonaws.com"
+        "Service": ["apigateway.amazonaws.com", "lambda.amazonaws.com"]
       },
       "Effect": "Allow",
       "Sid": ""
@@ -33,4 +37,85 @@ resource "aws_lambda_function" "test_lambda" {
       foo = "bar"
     }
   }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.lambda_logs,
+    aws_cloudwatch_log_group.example,
+  ]
+}
+
+resource "aws_cloudwatch_log_group" "example" {
+  name              = "/aws/lambda/${var.lambda_function_name}"
+  retention_in_days = 14
+}
+
+resource "aws_iam_policy" "lambda_logging" {
+  name        = "lambda_logging"
+  path        = "/"
+  description = "IAM policy for logging from a lambda"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "arn:aws:logs:*:*:*",
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.iam_for_lambda.name
+  policy_arn = aws_iam_policy.lambda_logging.arn
+}
+
+resource "aws_lambda_permission" "apigw_lambda" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.test_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn = "${replace(aws_api_gateway_deployment.example.execution_arn, aws_api_gateway_stage.example.stage_name, "")}*/*"
+}
+
+resource "aws_lambda_permission" "apigw_lambda_console_test" {
+  statement_id  = "AllowExecutionFromAPIGatewayConsole"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.test_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn = "${aws_api_gateway_rest_api.example.execution_arn}/*/POST/"
+}
+
+resource "aws_api_gateway_rest_api" "example" {
+  body = templatefile("openapi.yaml", {invocation_arn = aws_lambda_function.test_lambda.invoke_arn})
+
+  name = "serverless-discord"
+
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
+}
+
+resource "aws_api_gateway_deployment" "example" {
+  rest_api_id = aws_api_gateway_rest_api.example.id
+
+  triggers = {
+    redeployment = sha1(aws_api_gateway_rest_api.example.body)
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_api_gateway_stage" "example" {
+  deployment_id = aws_api_gateway_deployment.example.id
+  rest_api_id   = aws_api_gateway_rest_api.example.id
+  stage_name    = "example"
 }
