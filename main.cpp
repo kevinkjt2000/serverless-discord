578c7afe52f538bf8b48fa91c709b1cb91c7dadf
Kevin Tindall <kevinkjt2000@gmail.com>
2021-03-09 20:08:03 -0600

    Prove that aws lambda environment variables work

--- main.cpp
+++ main.cpp
@@ -1,11 +1,21 @@
+#include <cstdlib>
+#include <iostream>
+
 #include <aws/lambda-runtime/runtime.h>
 #include <nlohmann/json.hpp>
 
 using namespace aws::lambda_runtime;
 using json = nlohmann::json;
 
+std::string getEnvVar(std::string const & key)
+{
+    char * val = getenv( key.c_str() );
+    return val == NULL ? std::string("") : std::string(val);
+}
+
 static invocation_response my_handler(invocation_request const& req)
 {
+    std::cout << "env(foo): " << getEnvVar("foo") << std::endl;
     auto parsed_payload = json::parse(req.payload);
     if (parsed_payload.contains("type") && parsed_payload["type"] == 1) {
         auto response = R"(
